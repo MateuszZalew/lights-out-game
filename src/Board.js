b5d4676fd48417e0d1eda972631a876db1ed0bee
Mateusz Zalewski <zalewmat31@gmail.com>
2021-01-05 17:11:34 +0100

    add onClickEvent

--- src/Board.js
+++ src/Board.js
@@ -16,11 +16,48 @@ class Board extends Component {
     };
   }
 
+  flipCellsAround(coord) {
+    console.log("flipping");
+    let { ncols, nrows } = this.props;
+    let { board, hasWon } = this.state;
+    let [y, x] = coord.split("-").map(Number);
+
+    function flipCell(y, x) {
+      if (x >= 0 && x < ncols && y >= 0 && y < nrows) {
+        board[y][x] = !board[y][x];
+      }
+    }
+
+    flipCell(y, x);
+    flipCell(y, x + 1);
+    flipCell(y, x - 1);
+    flipCell(y + 1, x);
+    flipCell(y - 1, x);
+
+    let offNums = 0;
+    for (let x = 0; x < this.props.nrows; x++) {
+      for (let y = 0; y < this.props.ncols; y++) {
+        if (board[x][y] === true) {
+          offNums++;
+        }
+      }
+    }
+
+    if (offNums === 0) {
+      hasWon = true;
+    }
+
+    this.setState({
+      board,
+      hasWon,
+    });
+  }
+
   createBoard() {
     let board = [];
-    for (let i = 0; i < this.props.nrows; i++) {
+    for (let x = 0; x < this.props.nrows; x++) {
       let row = [];
-      for (let j = 0; j < this.props.ncols; j++) {
+      for (let y = 0; y < this.props.ncols; y++) {
         row.push(Math.random() < this.props.chanceLightStartsOn);
       }
       board.push(row);
@@ -29,22 +66,31 @@ class Board extends Component {
   }
 
   render() {
-
     let tableBoard = [];
-    for (let i = 0; i < this.props.nrows; i++) {
+    for (let x = 0; x < this.props.nrows; x++) {
       let row = [];
-      for (let j = 0; j < this.props.ncols; j++) {
-        let coord = `${i}-${j}`;
-        row.push(<Cell key={coord} isLit={this.state.board[i][j]} />);
+      for (let y = 0; y < this.props.ncols; y++) {
+        let coord = `${x}-${y}`;
+        row.push(
+          <Cell
+            key={coord}
+            isLit={this.state.board[x][y]}
+            flipCellsAroundMe={() => this.flipCellsAround(coord)}
+          />
+        );
       }
-      tableBoard.push(<tr key={i}>{row}</tr>);
+      tableBoard.push(<tr key={x}>{row}</tr>);
     }
 
     return (
       <div>
-        <table className="Board">
-          <tbody>{tableBoard}</tbody>
-        </table>
+        {this.state.hasWon ? (
+          <h1 className="Board-header">You win</h1>
+        ) : (
+          <table className="Board">
+            <tbody>{tableBoard}</tbody>
+          </table>
+        )}
       </div>
     );
   }
--- src/Cell.js
+++ src/Cell.js
@@ -2,12 +2,24 @@ import React, {Component} from 'react'
 import "./Cell.css"
 
 class Cell extends Component {
+  constructor(props) {
+    super(props);
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  flipCellsAroundMe = () => {
+
+  }
+
+  handleClick = (e) => {
+    this.props.flipCellsAroundMe();
+  }
 
   render() {
     let classes = "Cell" + (this.props.isLit ? " Cell-lit" : "");
 
     return (
-        <td className={classes}/>
+        <td className={classes} onClick={this.handleClick}/>
     )
   }
 }
