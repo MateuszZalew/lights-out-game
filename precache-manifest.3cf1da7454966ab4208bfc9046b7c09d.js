e7ca5cbc9098ecbfa3e7433ba1193f3cab31a3b4
Mateusz Zalewski <zalewmat31@gmail.com>
2021-01-06 11:49:24 +0100

    Updates

---
+++ precache-manifest.3cf1da7454966ab4208bfc9046b7c09d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "497a19f39a3a79804843",
+    "url": "/lights-out-game/static/css/main.cf70dafe.chunk.css"
+  },
+  {
+    "revision": "497a19f39a3a79804843",
+    "url": "/lights-out-game/static/js/main.497a19f3.chunk.js"
+  },
+  {
+    "revision": "b43a7eab4c27c21e54e5",
+    "url": "/lights-out-game/static/js/1.b43a7eab.chunk.js"
+  },
+  {
+    "revision": "c21eb99d7eac6d163be2",
+    "url": "/lights-out-game/static/js/runtime~main.c21eb99d.js"
+  },
+  {
+    "revision": "e2269ea14a744bd6f9c34c8e812f06e6",
+    "url": "/lights-out-game/index.html"
+  }
+];
